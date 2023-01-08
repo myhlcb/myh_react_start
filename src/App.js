6a1b4fc1f15ac3b3c39693552e8e8ff0ff4bdc87
S1mple <1029785965@qq.com>
2023-01-08 20:43:57 +0800

    feat():start

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from './pages/Layout';
+import Login from './pages/Login';
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return(<BrowserRouter>
+    <div className='App'>
+      <Routes>
+        <Route path='/login' element={<Login />}></Route>
+        <Route path='/layout' element={<Layout />}></Route>
+      </Routes>
     </div>
-  );
+  </BrowserRouter>)
 }
 
 export default App;
---
+++ src/pages/Layout/index.js
@@ -0,0 +1,9 @@
+import { Button } from 'antd';
+function Layout() {
+  return (
+    <div>
+      <Button type='primary'>Layout</Button>
+    </div>
+  );
+}
+export default Layout;
